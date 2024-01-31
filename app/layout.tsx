1ed167a484ae607810382098aa8ef37c599f7a24
xiansakana <2461298052@qq.com>
2024-01-30 21:23:51 -0500

    update name

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              storageKey="jotion-theme-2"
+              storageKey="pernotes-theme-2"
             >
               <Toaster position="bottom-center" />
               <ModalProvider />
